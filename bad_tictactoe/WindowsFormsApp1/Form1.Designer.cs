02404a1cebd87b8a0fbf166167bf833d828d618a
Osaki Tsukiko <104794884+OsakiTsukiko@users.noreply.github.com>
2022-11-08 13:33:06 +0200

    Add files via upload

---
+++ bad_tictactoe/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // printPreviewDialog1
+            // 
+            this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
+            this.printPreviewDialog1.Enabled = true;
+            this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
+            this.printPreviewDialog1.Name = "printPreviewDialog1";
+            this.printPreviewDialog1.Visible = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Location = new System.Drawing.Point(349, 59);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(150, 150);
+            this.panel1.TabIndex = 1;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::WindowsFormsApp1.Properties.Resources.board;
+            this.pictureBox1.Location = new System.Drawing.Point(126, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(540, 426);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(21, 45);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "RESTART";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ bad_tictactoe/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        bool iend = false;
+        bool turn = true;
+        int s_x = 0;
+        int s_y = 0;
+
+        int check_row (int j)
+        {
+            int s = 0;
+            for (int i = 1; i <= 3; i += 1)
+            {
+                Button a = (Button)panel1.Controls.Find("casuta_" + i + "_" + j, true)[0];
+                if (a.Text == "X") s += 1;
+                if (a.Text == "O") s -= 1;
+            }
+            if (s == -3) return -1;
+            if (s == 3) return 1;
+            return 0;
+        }
+
+        int check_col(int i)
+        {
+            int s = 0;
+            for (int j = 1; j <= 3; j += 1)
+            {
+                Button a = (Button)panel1.Controls.Find("casuta_" + i + "_" + j, true)[0];
+                if (a.Text == "X") s += 1;
+                if (a.Text == "O") s -= 1;
+            }
+            if (s == -3) return -1;
+            if (s == 3) return 1;
+            return 0;
+        }
+
+        int check_d()
+        {
+            Button Z = (Button)panel1.Controls.Find("casuta_" + 2 + "_" + 2, true)[0];
+            Button A11 = (Button)panel1.Controls.Find("casuta_" + 1 + "_" + 1, true)[0];
+            Button A33 = (Button)panel1.Controls.Find("casuta_" + 3 + "_" + 3, true)[0];
+            Button A13 = (Button)panel1.Controls.Find("casuta_" + 1 + "_" + 3, true)[0];
+            Button A31 = (Button)panel1.Controls.Find("casuta_" + 3 + "_" + 1, true)[0];
+            if (
+                Z.Text == A11.Text && A11.Text == A33.Text ||
+                Z.Text == A13.Text && A13.Text == A31.Text
+                )
+            {
+                if (Z.Text == "X") return 1;
+                if (Z.Text == "O") return -1;
+                return 0;
+            }
+
+            return 0;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            newGame();
+        }
+
+        Button[,] casuta = new Button[4, 4];
+        void newGame()
+        {
+            panel1.Controls.Clear();
+            for (int i = 1; i <= 3; i += 1)
+            {
+                for (int j = 1; j <= 3; j += 1)
+                {
+                    casuta[i, j] = new Button();
+                    casuta[i, j].Size = new Size(50, 50);
+                    casuta[i, j].Left = i * 50 - 50;
+                    casuta[i, j].Top = j * 50 - 50;
+                    // casuta[i, j].Text = i + ", " + j;
+                    casuta[i, j].Name = "casuta_" + i + "_" + j;
+                    casuta[i, j].Click += new EventHandler(casuta_click);
+                    panel1.Controls.Add(casuta[i, j]);
+                }
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void casuta_click(object sender, EventArgs e)
+        {
+            Button apasat = (Button)sender;
+            if (apasat.Text != "") return;
+            int x = Convert.ToInt16(apasat.Name.Substring(7, 1));
+            int y = Convert.ToInt16(apasat.Name.Substring(9, 1));
+            Console.WriteLine(x + " " + y);
+            apasat.Font = new Font("Arial", 20F);
+            if (turn)
+            {
+                apasat.Text = "X";
+                apasat.BackColor = Color.IndianRed;
+                apasat.ForeColor = Color.RoyalBlue;
+                // apasat.Enabled = false;
+            } else
+            {
+                apasat.Text = "O";
+                apasat.BackColor = Color.RoyalBlue;
+                apasat.ForeColor = Color.IndianRed;
+                // apasat.Enabled = false;
+            }
+            turn = !turn;
+        }
+
+        void XWIN()
+        {
+            if (iend) return;
+            iend = true;
+            s_x += 1;
+            MessageBox.Show("X WINS");
+            newGame();
+            turn = true;
+            iend = false;
+        }
+
+        void OWIN()
+        {
+            if (iend) return;
+            iend = true;
+            s_y += 1;
+            MessageBox.Show("O WINS");
+            newGame();
+            turn = true;
+            iend = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            for ( int i = 1; i <= 3; i += 1 )
+            {
+                if (check_row(i) == 1)
+                {
+                    XWIN();
+                }
+                if (check_row(i) == -1)
+                {
+                    OWIN();
+                }
+                if (check_col(i) == 1)
+                {
+                    XWIN();
+                }
+                if (check_col(i) == -1)
+                {
+                    OWIN();
+                }
+                if (check_d() == 1)
+                {
+                    XWIN();
+                }
+                if (check_d() == -1)
+                {
+                    OWIN();
+                }
+            }
+            label1.Text = "X: " + s_x + " | " + s_y + " :O";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            newGame();
+            turn = true;
+        }
+    }
+}
